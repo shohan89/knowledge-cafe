631207644214c0627c6a36b696672fcab60fe40e
shoh4n <maffin.killer007@gmail.com>
2025-07-22 17:08:00 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='font-bold text-5xl underline'>React Knowledge Kafe</h1>
+        </div>
+    );
+};
+
+export default Header;
